adc8f22cedc9812f25a6f6d4a03c9f8748836bfc
ComanGames <alfaa.gen@gmail.com>
2016-04-29 22:46:01 +0300

    We made simple bot logic

--- ComanJuiceBusinessCommunicator/Controllers/MessagesController.cs
+++ ComanJuiceBusinessCommunicator/Controllers/MessagesController.cs
@@ -18,20 +18,37 @@ namespace ComanJuiceBusinessCommunicator
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
+        private string[] _randomMessages= new string[] {"See our web page it is great coman-juice.com","You can find a lot of interesting information about us on our blog blog.coman-juice.com","Find tones of interesting examples of our work on gallery.coman-juice.com"};
         public async Task<Message> Post([FromBody]Message message)
         {
             if (message.Type == "Message")
             {
+                Random random = new Random();
                 // calculate something for us to return
-                int length = (message.Text ?? string.Empty).Length;
+                if (message.Text == null)
+                {
+                    return message.CreateReplyMessage($"Can I help you with anything ?");
+                }
+                if (message.Text == "Hello")
+                {
+                    return message.CreateReplyMessage("Hello my dear friend how can i help you?");
+                }
+                if (message.Text == "Fuck you")
+                {
+                    return message.CreateReplyMessage("It was rude sir. I hope You are better than that.");
+                }
+                return message.CreateReplyMessage(RandomMessage(random));
 
-                // return our reply to the user
-                return message.CreateReplyMessage($"You sent {length} characters");
             }
             else
             {
                 return HandleSystemMessage(message);
             }
+        }
+
+        private string RandomMessage(Random random)
+        {
+            return _randomMessages[random.Next(0,_randomMessages.Length-1)];
         }
 
         private Message HandleSystemMessage(Message message)
